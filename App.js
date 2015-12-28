33ede4f81232741693faafa1790a4adf72775924
Wu <wuzhaoyang@meizu.com>
2015-12-28 12:17:58 +0800

    add state

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {txt: 'asldfjlsdkj'}
+  }
+  update(e){
+    this.setState({txt: e.target.value})
+  }
    render(){
-       return <h1>saldfjkls {this.props.txt} {this.props.cat}</h1>
+       return (
+        <div>
+          <input type='text' onChange={this.update.bind(this)} />
+          <h1>saldfjkls {this.state.txt}</h1>
+       </div>
+       )
    }
 }
-App.defaultProps = {
-  txt: 'this is default'
-}
+// App.defaultProps = {
+//   txt: 'this is default'
+// }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
+// App.propTypes = {
+//   txt: React.PropTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
 export default App
 
 
