3d2d0bfb84609fbb060835c878462c81d4c1e765
Wu <wuzhaoyang@meizu.com>
2015-12-28 11:42:43 +0800

    add default props

--- App.js
+++ App.js
@@ -4,10 +4,13 @@ class App extends React.Component {
        return <h1>saldfjkls {this.props.txt} {this.props.cat}</h1>
    }
 }
+App.defaultProps = {
+  txt: 'this is default'
+}
 
 App.propTypes = {
   txt: React.PropTypes.string,
-  cat: React.PropTypes.number
+  cat: React.PropTypes.number.isRequired
 }
 export default App
 
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-ReactDOM.render(<App txt="heiheihei" cat= {8} />, document.getElementById('app'))
+ReactDOM.render(<App txt="heiheihei" cat={8} />, document.getElementById('app'))
